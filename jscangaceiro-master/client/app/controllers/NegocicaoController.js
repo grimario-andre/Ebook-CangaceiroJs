3ebbb3a0225556b5b87d7bca47d90037b45c8388
grimario-andre <andre02souza@outlook.com.br>
2020-10-02 21:26:33 -0300

    working with forms to resolve the problems with dates.

--- jscangaceiro-master/client/app/controllers/NegocicaoController.js
+++ jscangaceiro-master/client/app/controllers/NegocicaoController.js
@@ -1,25 +1,32 @@
 //Creating the class. Negociation Controller.
 class NegociacaoController {
     //creat a constructor.
-    constructor(){
-       //create vbarible to revice method document.queryselector to cross function bind()
-       let $ = document.querySelector.bind(document);
+    constructor() {
+        //create vbarible to revice method document.queryselector to cross function bind()
+        let $ = document.querySelector.bind(document);
 
-       this._inputData = $('#data');
-       this._inputQuantidade = $('#quantidade');
-       this._inputValor = $('#valor');
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
     }
-    
+
     //create method to add(event) for to cancel when to click in the bottom submit.
+   
     adiciona(event) {
         event.preventDefault();
-    
+
+        //relaese problem with atribute data.
+        //let data = new Date(this._inputData.value.split('-'));
+        
+        //Anymore to resolve the problem.
+        let data = new Date(this._inputData.value.replace(/-/g,','));
+        console.log(data);
+        
         let negociacao = new Negociacao(
             this._inputData.value,
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
         );
-
-        window.console.log(negociacao);
-    }
+        console.log(negociacao);
+    };
 };
